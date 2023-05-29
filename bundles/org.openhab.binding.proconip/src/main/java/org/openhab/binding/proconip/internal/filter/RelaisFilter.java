89d7e3454bd284c0e6293f2e2afee1898ddd0abd
Martin Renner <martin+github@planet-renner.net>
2023-05-29 21:55:43 +0200

    Minor fixes and restructuring of the ProConIp binding.
    Signed-off-by: Martin Renner <martin+github@planet-renner.net>

--- bundles/org.openhab.binding.proconip/src/main/java/org/openhab/binding/proconip/internal/filter/RelaisFilter.java
+++ bundles/org.openhab.binding.proconip/src/main/java/org/openhab/binding/proconip/internal/filter/RelaisFilter.java
@@ -8,7 +8,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.proconip.internal.relaisfilter;
+package org.openhab.binding.proconip.internal.filter;
 
 import java.util.HashMap;
 import java.util.Map;
